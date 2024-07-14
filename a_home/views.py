bd6a8bb96fbe3e60551b577d17bf0b50dd63e2e8
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-07-14 11:58:48 +0200

    Test message board

--- a_home/views.py
+++ a_home/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def home_view(request):
-    return render(request, 'home.html')
+    # return render(request, 'home.html')
+    return redirect('messageboard')
