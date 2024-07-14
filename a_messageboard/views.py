552f62a2eabdeae874c621a0148fce5f34274d1e
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-07-14 12:32:51 +0200

    Update views.py

--- a_messageboard/views.py
+++ a_messageboard/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from .models import Message, MessageBoard
 
 # Create your views here.
 
 @login_required
 def messageboard_view(request):
-    return render(request, 'a_messageboard/index.html')
+    messageboard = get_object_or_404(MessageBoard, id=1)
+
+    context = {
+        'messageboard': messageboard,
+    }
+    return render(request, 'a_messageboard/index.html', context)
