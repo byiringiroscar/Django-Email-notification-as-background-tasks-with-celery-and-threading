06633583e3867cf3ad0aa2ab5e5cad0c8d7e7a55
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-07-14 12:38:02 +0200

    Update views.py

--- a_messageboard/views.py
+++ a_messageboard/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Message, MessageBoard
+from .models import *
+from .forms import *
 
 # Create your views here.
 
