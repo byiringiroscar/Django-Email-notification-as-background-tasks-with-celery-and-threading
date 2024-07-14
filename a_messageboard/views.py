f756c60462485c650c20f511e49fe3512a7c0dca
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-07-14 12:40:01 +0200

    Update views.py

--- a_messageboard/views.py
+++ a_messageboard/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import *
 from .forms import *
@@ -8,6 +8,16 @@ from .forms import *
 @login_required
 def messageboard_view(request):
     messageboard = get_object_or_404(MessageBoard, id=1)
+    form = MessageCreateForm()
+
+    if request.method == 'POST':
+        form = MessageCreateForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.messageboard = messageboard
+            message.author = request.user
+            message.save()
+            return redirect('messageboard')
 
     context = {
         'messageboard': messageboard,
