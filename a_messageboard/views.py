91336ce1e50735290b11a3778cde514abf6c6269
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-07-14 11:40:26 +0200

    Add main board

--- a_messageboard/views.py
+++ a_messageboard/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required
+def messageboard_view(request):
+    return render(request, 'a_messageboard/index.html')
