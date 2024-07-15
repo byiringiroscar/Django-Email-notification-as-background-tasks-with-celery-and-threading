6a0eb54e59576bfcb7e3676757db8c98e99e7b0a
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-07-15 15:32:07 +0200

    Update tasks.py

--- a_messageboard/tasks.py
+++ a_messageboard/tasks.py
@@ -1,7 +1,8 @@
 from celery import shared_task
 from django.core.mail import EmailMessage
 
-@shared_task
-def send_email_taks(subject, body, email):
-    email = EmailMessage(subject, body, to=[email])
+@shared_task(name='email_notification')
+def send_email_taks(subject, body, email_address):
+    email = EmailMessage(subject, body, to=[email_address])
     email.send()
+    return email_address
